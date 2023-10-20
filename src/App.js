ce0a675c9387a07f43c26a98bc4d8d666024252c
tahaKhan2003 <141918318+tahaKhan2003@users.noreply.github.com>
2023-10-20 17:41:41 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
 import LandingPage from './Components/LandingPage';
+import 'bootstrap/dist/css/bootstrap.css'; 
 
+import Cap from './Components/Cap';
 function App() {
   return (
     <div className="App">
       <LandingPage/>
+    <Cap/>
     </div>
   );
 }
---
+++ src/Components/Cap.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css'; 
+import React from 'react';
+
+const Cap = () => {
+    return (
+        <div>
+            <h1>abc</h1>
+        </div>
+    );
+};
+
+export default Cap;
